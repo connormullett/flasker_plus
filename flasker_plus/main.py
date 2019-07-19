52fa31b3ca5c10d90c5d4baf610f7c67bdbadc20
connor <connormullett@gmail.com>
2019-07-19 10:00:42 -0400

    finished main command

---
+++ flasker_plus/main.py
@@ -0,0 +1,23 @@
+
+import click
+import os
+
+
+TEMPLATE_URL = 'https://github.com/XzavierDunn/restPlus.git'
+
+
+@click.group()
+def main():
+    pass
+
+
+@main.command()
+def start():
+    project_name = click.prompt('Enter project Name: ')
+
+    path = os.getcwd()
+
+    install_path = f'{path}/{project_name}'
+
+    os.system('git clone %s %s' % (TEMPLATE_URL, install_path))
+
